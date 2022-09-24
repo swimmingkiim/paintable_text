8621458131748a228676936aa0724faf3b3cab62
swimmingkiim <swimmingkiim@gmail.com>
2022-09-24 21:30:42 +0900

    Update with editable_text_painter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,121 @@
+// dart
+import 'dart:math';
+
+// flutter
 import 'package:flutter/material.dart';
+
+// packages
+import 'package:editable_text_painter/editable_text_painter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo for Draggable Text Canvas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const DraggableTextCanvas(
+          title: 'Flutter Demo for Draggable Text Canvas'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class DraggableTextCanvas extends StatefulWidget {
+  const DraggableTextCanvas({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DraggableTextCanvas> createState() => _DraggableTextCanvasState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DraggableTextCanvasState extends State<DraggableTextCanvas> {
+  final EditableTextController controller = EditableTextController(
+    text: 'hello world!',
+    textStyle: const TextStyle(
+      fontSize: 20.0,
+      color: Colors.black,
+    ),
+  );
 
-  void _incrementCounter() {
+  void _down(DragDownDetails details) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (controller.isHover(details.localPosition)) {
+        controller.moving = true;
+      } else {
+        controller.moving = false;
+      }
     });
+  }
+
+  void _up() {
+    setState(() {
+      controller.moving = false;
+    });
+  }
+
+  void _move(DragUpdateDetails details, BoxConstraints constraints) {
+    if (controller.moving) {
+      setState(() {
+        final double x = max(
+            0,
+            min(controller.position.x + details.delta.dx,
+                controller.canvasConstraints.constrainWidth()));
+        final double y = max(
+            0,
+            min(controller.position.y + details.delta.dy,
+                controller.canvasConstraints.constrainHeight()));
+        controller.updatePosition(Offset(x, y));
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: LayoutBuilder(builder: (context, constraints) {
+        return GestureDetector(
+          onPanDown: (event) {
+            _down(event);
+          },
+          onPanEnd: (details) {
+            _up();
+          },
+          onPanUpdate: (details) {
+            _move(details, constraints);
+          },
+          onTapUp: (TapUpDetails details) {
+            setState(() {
+              if (controller.isHover(details.localPosition)) {
+                controller.editing = true;
+              } else {
+                controller.editing = false;
+              }
+            });
+          },
+          child: Container(
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+            color: Colors.grey,
+            child: CustomPaint(
+              foregroundPainter: EditableTextTextPainter(
+                controller: controller..canvasConstraints = constraints,
+              ),
+              size: Size(
+                MediaQuery.of(context).size.width,
+                MediaQuery.of(context).size.height,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        );
+      }), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }

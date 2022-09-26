80127d3a18d4a6c50e3c62677bf1be2fed1864c8
swimmingkiim <swimmingkiim@gmail.com>
2022-09-26 11:09:19 +0900

    Remove unused field

--- lib/src/editable_text_controller/editable_text_position.dart
+++ lib/src/editable_text_controller/editable_text_position.dart
@@ -4,7 +4,6 @@ import 'package:vector_math/vector_math.dart' show Vector2;
 class EditableTextPosition {
   EditableTextPosition({required this.position});
 
-  bool moving = false;
   final Vector2 position;
 
   double get x => position.x;
@@ -14,10 +13,6 @@ class EditableTextPosition {
   set x(double x) => position.x = x;
 
   set y(double y) => position.y = y;
-
-  void startMoving() => moving = true;
-
-  void endMoving() => moving = false;
 
   void addVector2(Vector2 vector2) => position.add(vector2);
 }

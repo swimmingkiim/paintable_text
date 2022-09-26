259fad0efe0436971cccf346ee10ed80634459fa
swimmingkiim <swimmingkiim@gmail.com>
2022-09-26 11:09:14 +0900

    Remove unused field

--- lib/src/editable_text_controller/editable_text_data.dart
+++ lib/src/editable_text_controller/editable_text_data.dart
@@ -1,12 +1,7 @@
 class EditableTextData {
   EditableTextData({required this.textData});
 
-  bool editing = false;
   String textData;
-
-  void startEditing() => editing = true;
-
-  void endEditing() => editing = false;
 
   void updateTextData(String text) => textData = text;
 }
